48ebad2f88c2739d5aeed5e7369bf53bd9cf14bb
Miguel Montoya <miguel@invorious.com>
2023-09-12 20:16:40 -0500

    feat: create api-shared lib

---
+++ apps/shared/api-interfaces/src/lib/auth/index.ts
@@ -0,0 +1,4 @@
+export * from './registration-request';
+export * from './login-request';
+export * from './jwt-payload';
+export * from './verify-user-response';
---
+++ apps/shared/api-interfaces/src/lib/auth/verify-user-response.ts
@@ -0,0 +1,3 @@
+export interface VerifyUserResponse {
+  access_token: string;
+}

2a77562e78741033d1bbab40997476a7195a02db
Miguel Montoya <miguel@invorious.com>
2023-10-15 01:08:58 -0500

    style: move styles to loader component

--- apps/main/src/app/modules/ui/components/loader/loader.tsx
+++ apps/main/src/app/modules/ui/components/loader/loader.tsx
@@ -1,5 +1,16 @@
+import styles from './loader.module.scss';
+import cn from 'classnames';
 import { CircularProgress } from '@mui/material';
 
-export function Loader() {
-  return <CircularProgress color="inherit" />;
+export function Loader(props: { dialog?: boolean }) {
+  const { dialog = false } = props;
+  return (
+    <div
+      className={cn(styles['loader'], {
+        [styles['dialog']]: dialog,
+      })}
+    >
+      <CircularProgress color="inherit" />
+    </div>
+  );
 }
